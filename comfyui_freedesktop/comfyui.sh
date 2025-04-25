f8ff72974a7f25a2373bc3a298d4852b2615760e
efenstor <megester@efenstor.net>
2025-04-25 20:35:02 +0700

    Much improved comfyui.sh with normal command line arguments, help, etc.

--- comfyui_freedesktop/comfyui.sh
+++ comfyui_freedesktop/comfyui.sh
@@ -1,27 +1,74 @@
 #!/bin/sh
+# copyleft 2025 Efenstor
 
-# Command line arguments:
-# --no-auto-launch: do not auto-launch the browser
+# Run with -h or -? to display help
 
+# Defaults
+# (use anything for True or nothing for False)
 reserve_vram=0.9
 max_split_size=512
 preview_method=auto
+auto_launch=1
+garbage_collector=1
 
 cd "$(dirname $0)"
 
-read -p "Use --lowvram? (needed only if you get the \`HIP out of memory\` errors) y/N " a
-if [ "$a" = 'y' ]; then
-  lowvmem="--lowvram"
+# Parse the named parameters
+optstr="?hilar:s:p:"
+while getopts $optstr opt
+do
+  case "$opt" in
+    h|\?) help=1 ;;
+    i) interactive=1 ;;
+    l) lowvram="--lowvram" ;;
+    a) auto_launch= ;;
+    g) garbage_collector= ;;
+    r) reserve_vram="$OPTARG" ;;
+    s) max_split_size="$OPTARG" ;;
+    p) preview_method="$OPTARG" ;;
+    :) echo "Missing argument for -$OPTARG" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $(expr $OPTIND - 1)
+
+# Help
+if [ "$help" ]; then
+  echo "Command line arguments:"
+  echo "-h or -?: show this help"
+  echo "-i: interactive mode"
+  echo "-l: use --lowvram (needed only if you get the \`HIP out of memory\` errors)"
+  echo "-a: don't auto-launch browser"
+  echo "-g: don't use garbage collector"
+  echo "-r: garbage collector reserve_vram size (default=$reserve_vram)"
+  echo "-s: garbage collector max_split_size size (default=$reserve_vram)"
+  echo "-p: preview method (default=$preview_method)"
+  exit
 fi
 
-if [ "$1" != "--no-auto-launch" ]; then
+# Replace bool with actual parameters
+if [ "$garbage_collector" ]; then
+  garbage_collector="PYTORCH_HIP_ALLOC_CONF=garbage_collection_threshold:$reserve_vram,max_split_size_mb:$max_split_size"
+fi
+if [ "$auto_launch" ]; then
   auto_launch="--auto-launch"
 fi
 
-PYTORCH_HIP_ALLOC_CONF=garbage_collection_threshold:$reserve_vram,max_split_size_mb:$max_split_size \
-  bin/python ComfyUI/main.py \
-    $lowvmem \
-    --reserve-vram $reserve_vram \
-    --preview-method $preview_method \
-    $auto_launch
+# Interactive questions
+if [ "$interactive" ]; then
+  read -p "Use --lowvram? (needed only if you get the \`HIP out of memory\` errors) y/N " a
+  if [ "$a" = 'y' ]; then
+    lowvram="--lowvram"
+  else
+    lowvram=
+  fi
+fi
+
+# Execute
+env $garbage_collector bin/python ComfyUI/main.py \
+  $lowvram \
+  --reserve-vram $reserve_vram \
+  --preview-method $preview_method \
+  $auto_launch
 
