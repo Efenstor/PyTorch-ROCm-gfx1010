661d528a9137f39bf220c503a9ab0ef1f861996d
efenstor <megester@efenstor.net>
2025-05-29 23:45:11 +0700

    comfyui.sh: * separate reserve_vram (-r) and garbage collection threshold (-t) options * added -d (disable smart memory) option (may help against OOM errors)

--- comfyui_freedesktop/comfyui.sh
+++ comfyui_freedesktop/comfyui.sh
@@ -5,7 +5,8 @@
 
 # Defaults
 # (use anything for True or nothing for False)
-reserve_vram=0.9
+reserve_vram=0.5
+gc_threshold=0.9
 max_split_size=512
 preview_method=auto
 auto_launch=1
@@ -13,21 +14,24 @@ garbage_collector=1
 attention="--use-split-cross-attention"
 interactive=
 lowvram=
+dsm=
 custom=
 
 cd "$(dirname $0)"
 
 # Parse the named parameters
-optstr="?hilagr:s:p:qc:"
+optstr="?hildagr:t:s:p:qc:"
 while getopts $optstr opt
 do
   case "$opt" in
     h|\?) help=1 ;;
     i) interactive=1 ;;
     l) lowvram="--lowvram" ;;
+    d) dsm="--disable-smart-memory" ;;
     a) auto_launch= ;;
     g) garbage_collector= ;;
     r) reserve_vram="$OPTARG" ;;
+    t) gc_threshold="$OPTARG" ;;
     s) max_split_size="$OPTARG" ;;
     p) preview_method="$OPTARG" ;;
     q) attention="--use-quad-cross-attention" ;;
@@ -46,11 +50,13 @@ if [ "$help" ]; then
   echo "Arguments:"
   echo "-h or -?: show this help"
   echo "-i: interactive mode"
-  echo "-l: use --lowvram (needed only if you get the \`HIP out of memory\` errors)"
+  echo "-l: use --lowvram (makes everything very slow but fights OOM errors)"
+  echo "-d: disable smart memory (same as -l but different strategy)"
   echo "-a: don't auto-launch browser"
   echo "-g: don't use garbage collector"
-  echo "-r: garbage collector reserve_vram size (default=$reserve_vram)"
-  echo "-s: garbage collector max_split_size size (default=$max_split_size)"
+  echo "-r: reserve_vram size (GB, default=$reserve_vram)"
+  echo "-t: garbage collector threshold (0..1 VRAM, default=$gc_threshold)"
+  echo "-s: garbage collector max_split_size size (MB, default=$max_split_size)"
   echo "-p: preview method (default=$preview_method)"
   echo "-q: use sub-quadratic cross attention (may be slower)"
   echo "-c: custom parameters to be added to the ComfyUI command line"
@@ -60,7 +66,7 @@ fi
 
 # Replace bool with actual parameters
 if [ "$garbage_collector" ]; then
-  garbage_collector="PYTORCH_HIP_ALLOC_CONF=garbage_collection_threshold:$reserve_vram,max_split_size_mb:$max_split_size,expandable_segments:True"
+  garbage_collector="PYTORCH_HIP_ALLOC_CONF=garbage_collection_threshold:$gc_threshold,max_split_size_mb:$max_split_size,expandable_segments:True"
 fi
 if [ "$auto_launch" ]; then
   auto_launch="--auto-launch"
@@ -79,6 +85,7 @@ fi
 # Execute
 env $garbage_collector bin/python ComfyUI/main.py \
   $lowvram \
+  $dsm \
   --reserve-vram $reserve_vram \
   --preview-method $preview_method \
   --fp8_e4m3fn-unet --fp8_e4m3fn-text-enc --bf16-vae \
